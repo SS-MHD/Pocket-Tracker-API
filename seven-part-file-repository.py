eb6109054fb350bddaf8218a49933b4efc71988d
SS-MHD <CVANU100@GMAIL.COM>
2026-07-03 16:32:08 +0330

    Update seven-part-file-repository.py

--- seven-part-file-repository.py
+++ seven-part-file-repository.py
@@ -10,6 +10,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# Developed by: [M.Sh]
+
 DATABASE_URL = "sqlite:///./spendly.db"
 
 engine = create_engine(
